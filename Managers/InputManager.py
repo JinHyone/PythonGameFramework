c4b2518898ed128296ec6bbe7337f100374c3bc9
DaRoED <104150579+DaRoED@users.noreply.github.com>
2024-08-30 23:45:52 +0900

    주석 추가 및 인스턴스 생성 때 동시에 초기화

--- Managers/InputManager.py
+++ Managers/InputManager.py
@@ -6,17 +6,22 @@ from pygame import event, quit, QUIT, KEYDOWN, KEYUP
 
 
 class InputManager:
-	key: Dict[str, bool] = dict()
+	key: Dict[str, bool]
 	instance: 'InputManager' = None
+
+	def init(self):
+		self.key = dict()
 
 	@staticmethod
 	# GetInstance Method
 	def GI():
 		if InputManager.instance is None:
 			InputManager.instance = InputManager()
+			InputManager.instance.init()
 
 		return InputManager.instance
 
+	# 일반적인 키는  'd' 'a' 꼴로 사용 가능하지만 쉬프트 등의 입력은 pygame.xxx로 사용해야함
 	def KeyPressed(self, key_type):
 		return self.key.get('d')
 
