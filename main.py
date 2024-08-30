ac9bba9e5cdf141e1eacc0946e3abe98539d9b74
DaRoED <104150579+DaRoED@users.noreply.github.com>
2024-08-30 23:58:48 +0900

    기능 테스트

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from pygame import init, display
 from pygame.color import THECOLORS
 from Managers.InputManager import InputManager
+from Managers.TimeManager import TimeManager
 
 # constant config
 FPS: int = 60
 
-init()
+init() # pygame init
+TimeManager.GI().setFPS(250)
 
 GameDisplay = display.set_mode((1080, 720))
 GameDisplay.fill(THECOLORS['white'])
@@ -20,5 +22,10 @@ def GameMain():
 while True:
 	display.update()
 	InputManager.GI().update()
+	TimeManager.GI().update()
+
+	if InputManager.GI().KeyPressed('d'):
+		print('fps: {} delta: {}'.format(TimeManager.GI().getFrame(), TimeManager.GI().getDelta()))
+
 	GameMain()
 
