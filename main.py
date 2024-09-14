66b978dc8507aded92690a750014e9d5ce5ab590
DaRoED <104150579+DaRoED@users.noreply.github.com>
2024-09-14 14:47:57 +0900

    더블 버퍼링 및 이미지 불러오기 테스트

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
+import pygame
 from pygame import init, display
 from pygame.color import THECOLORS
-from Scene.Scene import SCENE_TYPE
 from Managers.InputManager import InputManager
 from Managers.TimeManager import TimeManager
 from Managers.SceneManager import SceneManager
 
 # constant config
 FPS: int = 60
+display_width = 1280
+display_height = 720
 
 init()  # pygame init
-TimeManager.GI().setFPS(250)
+TimeManager.GI().setFPS(FPS)
 
-GameDisplay = display.set_mode((1080, 720))
+GameDisplay = display.set_mode((display_width, display_height), pygame.DOUBLEBUF | pygame.FULLSCREEN | pygame.HWSURFACE)
 GameDisplay.fill(THECOLORS['white'])
 
 display.set_caption('Game')
 
+background = pygame.transform.scale(pygame.image.load('E:/PythonProject/HanyangGameProject/Resources/py_back_HD.jpg'), (display_width, display_height))
+
 
 def GameMain():
-	GameDisplay.fill(THECOLORS['white'])
+	GameDisplay.blit(background, (0, 0))
+	display.flip()
 
 
 while True:
@@ -27,8 +32,7 @@ while True:
 	SceneManager.GI().update()
 	SceneManager.GI().render(GameDisplay)
 
-	if InputManager.GI().KeyPressed('d'):
+	if InputManager.GI().KeyPressed('f'):
 		print('fps: {} delta: {}'.format(TimeManager.GI().getFrame(), TimeManager.GI().getDelta()))
 
 	GameMain()
-	display.update()
