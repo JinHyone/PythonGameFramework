3dc6e49289f74fbd25825b83ddfe82e4ef063aaa
DaRoED <104150579+DaRoED@users.noreply.github.com>
2024-09-20 23:59:03 +0900

    리소스 기반

---
+++ Resources/2D/Texture.py
@@ -0,0 +1,34 @@
+from pygame.image import load
+from pygame.surface import Surface
+from pygame.color import Color
+
+
+class Texture:
+	surface: Surface
+	transparent: Color | tuple[int, int, int] = (0, 255, 0)
+	width: int
+	height: int
+
+	def loadImage(self, path: str) -> 'Texture':
+		self.surface = load(path)
+		size = self.surface.get_size()
+
+		self.width = size[0]
+		self.height = size[1]
+
+		return self
+
+	def getSurface(self) -> Surface:
+		return self.surface
+
+	def getWidth(self):
+		return self.width
+
+	def getHeight(self):
+		return self.height
+
+	def setTransparent(self, color: Color | tuple[int, int, int]):
+		self.transparent = color
+
+	def getTransparent(self):
+		return self.transparent
---
+++ Resources/ResourceBase.py
@@ -0,0 +1,6 @@
+class ResourceBase:
+	def loadFile(self, path: str):
+		pass
+
+	def saveFile(self, path: str):
+		pass
